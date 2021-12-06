d518ebd1299fc427a713c5fd811568cdae38b4a6
Jeffrey Beckman <jefflbeckman@gmail.com>
2021-12-05 19:57:19 -0600

    day 2 complete

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
 use anyhow::Result;
-use std::collections::VecDeque;
-use std::iter::FromIterator;
+
+#[derive(Debug, PartialEq, Eq)]
+enum SubOrder {
+    forward(i32),
+    down(i32),
+    up(i32),
+}
+
+#[derive(Debug)]
+struct Sub {
+    depth: i32,
+    distance: i32,
+    aim: i32,
+}
+
+impl From<&str> for SubOrder {
+    fn from(i: &str) -> Self {
+        let mut iter = i.split_whitespace();
+        //println!("{}", i);
+        match iter.next().unwrap() {
+            "forward" => SubOrder::forward(iter.next().expect("missing value").parse().unwrap()),
+            "down" => SubOrder::down(iter.next().expect("missing value").parse().unwrap()),
+            "up" => SubOrder::up(iter.next().expect("missing value").parse().unwrap()),
+            _ => SubOrder::forward(0),
+        }
+    }
+}
 
 fn main() -> Result<()>{
-    let file_contents: String = std::fs::read_to_string("inputs/day1.txt")?;
-    let mut depths_cache: VecDeque<i32> = VecDeque::new();
-    depths_cache.push_back(0);
-    depths_cache.push_back(0);
-    depths_cache.push_back(0);
-    let mut depth_increase_count : i32 = 0;
-    let parser = |s: &str| -> i32 {
-        let cur_depth : i32 = s.parse().unwrap();
-        let last_depth_window : i32 = depths_cache.iter().sum::<i32>();
-        let ret = depths_cache.pop_front().unwrap();
-        depths_cache.push_back(cur_depth);
-        println!("{:?}, {}, {}", depths_cache, depths_cache.iter().sum::<i32>(), last_depth_window);
-        if depths_cache.iter().sum::<i32>() > last_depth_window {
-            //println!("{} > {}", prev_depths.iter().sum::<i32>(), last_depth_window);
-            depth_increase_count = depth_increase_count + 1;
+    let file_contents: String = std::fs::read_to_string("inputs/day2.txt")?;
+    let orders : Vec<SubOrder> = file_contents.lines().map(|x| SubOrder::from(x)).collect();
+    let mut sub = Sub { depth: 0, distance: 0, aim: 0 };
+    orders.iter().for_each(|x| -> _ {
+            match x {
+                SubOrder::forward(d) => {
+                    sub.distance = sub.distance + d;
+                    sub.depth = sub.depth + (sub.aim * d)
+                }
+                SubOrder::down(d) => sub.aim = sub.aim + d,
+                SubOrder::up(d) => sub.aim = sub.aim - d,
             };
-        ret
-    };
-    let depths : Vec<i32> = file_contents.split_whitespace().map(parser).collect();
-    println!("{}", depth_increase_count);
+            println!("{:?}, {:?}", x, sub);
+        }
+    );
+    println!("{:?} {}",sub, sub.depth * sub.distance);
+
     Ok(())
 }
